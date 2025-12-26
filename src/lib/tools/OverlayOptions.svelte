af3032e5d33e50861bc7eba02717e84f3fee9fa3
Adi Ron <adi@adiron.me>
2025-12-26 17:48:55 +0100

    Overlay options

--- src/lib/tools/OverlayOptions.svelte
+++ src/lib/tools/OverlayOptions.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
   import Button from "../Button.svelte";
-    import IncrementInput from "../IncrementInput.svelte";
+  import IncrementInput from "../IncrementInput.svelte";
   import OverlayModal from "../OverlayModal.svelte";
-    import Slider from "../Slider.svelte";
-    import { globalState } from "../state.svelte";
+  import Slider from "../Slider.svelte";
+  import { globalState } from "../state.svelte";
   import type { OverlayTool } from "./OverlayTool.svelte";
 
   let { tool: _tool }: { tool: OverlayTool } = $props();
   let open = $state(false);
 
-  const sliderFormat = (n: number) =>  (n * 100).toFixed(0) + "%";
+  const sliderFormat = (n: number) => (n * 100).toFixed(0) + "%";
 </script>
 
 <OverlayModal {open} onclose={() => (open = false)} />
@@ -17,11 +17,23 @@
 <div class="tool-options">
   <Button text="Set overlay image" onclick={() => (open = true)} />
   {#if globalState.image.data}
-    <Button text="Clear overlay" onclick={() => (globalState.image.data = null)} />
+    <Button
+      text="Clear overlay"
+      onclick={() => (globalState.image.data = null)}
+    />
+    Scale
+    <IncrementInput
+      min={0.01}
+      max={10}
+      bind:value={globalState.image.scale}
+      increments={0.1}
+    />
+    Opacity
+    <Slider
+      formatFunction={sliderFormat}
+      min={0}
+      max={1}
+      bind:value={globalState.image.opacity}
+    />
   {/if}
-  Scale
-  <IncrementInput min={0.01} max={10} bind:value={globalState.image.scale} increments={0.1} />
-  Opacity
-  <Slider formatFunction={sliderFormat} min={0} max={1} bind:value={globalState.image.opacity} />
 </div>
-
